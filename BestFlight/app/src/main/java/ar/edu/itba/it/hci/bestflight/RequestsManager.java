82b5d64ce3b8fab42f5e5780bd3e1057dd7ea22b
santiagoburgos <burgossantiago94@gmail.com>
2017-06-23 19:11:33 -0300

    status

--- BestFlight/app/src/main/java/ar/edu/itba/it/hci/bestflight/RequestsManager.java
+++ BestFlight/app/src/main/java/ar/edu/itba/it/hci/bestflight/RequestsManager.java
@@ -2,7 +2,8 @@ package ar.edu.itba.it.hci.bestflight;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.support.v4.util.LruCache;
+import android.util.LruCache;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
--- BestFlight/app/src/main/java/ar/edu/itba/it/hci/bestflight/StatusFragment.java
+++ BestFlight/app/src/main/java/ar/edu/itba/it/hci/bestflight/StatusFragment.java
@@ -9,6 +9,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -21,13 +25,21 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class StatusFragment extends Fragment {
 
 
     private ProgressDialog progressDialog;
-    private ArrayList<String> airlinesId;
+    private Button searchButton;
+    private Spinner airlinesSpinner;
+    private EditText flightNumberET;
+    private Map<String, String> airlinesMap;
+    ArrayAdapter<String> adapter;
+    List<String> airlines;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,12 +54,76 @@ public class StatusFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        airlinesId = new ArrayList<String>();
         getAirlines();
+        airlinesMap = new HashMap<String, String>();
+        airlines = new ArrayList<String>();
+
+        searchButton = (Button) getView().findViewById(R.id.searchButton);
+        flightNumberET = (EditText) getView().findViewById(R.id.flightNumberET);
+        airlinesSpinner = (Spinner) getView().findViewById(R.id.airlinesSpinner);
+
+        if (searchButton != null) {
+            searchButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   checkStatus();
+                }
+            });
+
+        }
+
+
 
 
 
     }
+
+    private void checkStatus(){
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("loading"); // SETEAR LOS STRINGS Y LLAMARLOS TIPO getActivity().getString(R.string.loading_airlines)
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        String flightN = flightNumberET.getText().toString();
+        String airline = airlinesMap.get(airlinesSpinner.getSelectedItem().toString());
+
+        String  url = "http://hci.it.itba.edu.ar/v1/api/status.groovy?method=getflightstatus&airline_id=" +airline+ "&flight_number=" + flightN ;
+
+        //Log.d("url", url);
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                   if (response.has("status") ) {
+
+                       String status = response.getJSONObject("status").getString("status");
+
+                   }
+                   else{
+                       Toast.makeText(getActivity(), response.getJSONObject("error").getString("message") , Toast.LENGTH_LONG).show();
+                   }
+                    progressDialog.dismiss();
+                } catch (JSONException e) {
+                    Toast.makeText(getActivity(), "error" , Toast.LENGTH_LONG).show();
+                    progressDialog.dismiss();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), "error", Toast.LENGTH_LONG).show();
+                progressDialog.dismiss();
+            }
+        });
+        RequestsManager.getInstance(getActivity()).addToRequestQueue(jsObjRequest);
+
+    }
+
+
 
     private void getAirlines() {
         progressDialog = new ProgressDialog(getActivity());
@@ -66,9 +142,16 @@ public class StatusFragment extends Fragment {
 
                     // ACA SE TRABAJA CON LOS ELEMENTOS DEL JSON
                    for(int i=0; i<response.getJSONArray("airlines").length(); i++) {
-                        airlinesId.add((response.getJSONArray("airlines").getJSONObject(i)).getString("id"));
 
+                       airlinesMap.put(response.getJSONArray("airlines").getJSONObject(i).getString("name"),response.getJSONArray("airlines").getJSONObject(i).getString("id") );
+                       airlines.add(response.getJSONArray("airlines").getJSONObject(i).getString("name"));
                     }
+
+                    adapter = new ArrayAdapter<String>(getActivity(),
+                            android.R.layout.simple_spinner_item, airlines);
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    airlinesSpinner.setAdapter(adapter);
+
                     progressDialog.dismiss();
                 } catch (JSONException e) {
                     Toast.makeText(getActivity(), "error" , Toast.LENGTH_LONG).show();
