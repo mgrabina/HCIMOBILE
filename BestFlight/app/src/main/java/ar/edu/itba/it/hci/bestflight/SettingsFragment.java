f224c54c3de1f0d504c6d6e315ca214cff98b1af
Martin Grabina <mgrabina@itba.edu.ar>
2017-06-23 20:08:46 -0300

    detalles settings

--- BestFlight/app/src/main/java/ar/edu/itba/it/hci/bestflight/SettingsFragment.java
+++ BestFlight/app/src/main/java/ar/edu/itba/it/hci/bestflight/SettingsFragment.java
@@ -17,14 +17,26 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static android.widget.Toast.LENGTH_LONG;
 
 
 public class SettingsFragment extends Fragment {
     private Integer[] intervalos;
+    private String displayLanguage;
+    private Integer interval;
+
+    public String getDisplayLanguage() {
+        return displayLanguage;
+    }
+
+    public Integer getInterval() {
+        return interval;
+    }
 
     @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
@@ -32,25 +44,28 @@ public class SettingsFragment extends Fragment {
     }
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        displayLanguage = Locale.getDefault().getDisplayLanguage();
 
-        Spinner s = (Spinner) getActivity().findViewById(R.id.selectorDeTiempo);
+
+        final Spinner s = (Spinner) getActivity().findViewById(R.id.selectorDeTiempo);
         this.intervalos = new Integer[]{
                 1, 15, 30, 60, 120
         };
-        final int intervalos = Log.e("intervalos", this.intervalos.toString());
-        final int a = Log.e("activity", getActivity().toString());
+
 
 
         final ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(getActivity(), android.R.layout.simple_spinner_item, this.intervalos);
-        final int adap = Log.e("ADAPTER", adapter.toString());
-        final int spppp = Log.e("s", s.toString());
+
+        //Default
+        interval = 60;
+        s.setSelection(3);
 
         s.setAdapter(adapter);
         s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 //Si selecciono uno
-                Toast.makeText(getContext(), SettingsFragment.this.intervalos[position].toString(), LENGTH_LONG);
+                interval = intervalos[s.getSelectedItemPosition()];
             }
 
             @Override
@@ -61,11 +76,20 @@ public class SettingsFragment extends Fragment {
         ToggleButton toggle = (ToggleButton) getActivity().findViewById(R.id.selectorIdioma);
         toggle.setTextOff("EN");
         toggle.setTextOn("ES");
-        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+        if (displayLanguage.equals("EN"))
+            toggle.setChecked(true);
+        else
+            toggle.setChecked(false);
+
+            toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 //Si cambie el idioma
-                Toast.makeText(getContext(), ((Boolean)isChecked).toString(), LENGTH_LONG);
+                if(isChecked)
+                    displayLanguage = "ES";
+                else
+                    displayLanguage = "EN";
             }
         });
 
