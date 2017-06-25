e539a92926e6dd56bf1e6a3348c97873f49094d9
santiagoburgos <burgossantiago94@gmail.com>
2017-06-25 00:26:01 -0300

    :+1:

--- BestFlight/app/src/main/java/ar/edu/itba/it/hci/bestflight/MainActivity.java
+++ BestFlight/app/src/main/java/ar/edu/itba/it/hci/bestflight/MainActivity.java
@@ -135,6 +135,7 @@ public class MainActivity extends AppCompatActivity
 
     public void setNotificationInterval(long number){
         this.notificationInterval = number;
+        start();
     }
 
     public void start() {
